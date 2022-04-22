680dc0b450c8237b099649933544c59b46c11045
Samuel Jones <samuel.jones@stfc.ac.uk>
2022-04-22 13:33:56 +0100

    Initial investigation

---
+++ Wrappers/javascript/JS-course/Getting Started/05/demos/before/js-demo/home.js
@@ -0,0 +1,29 @@
+// let mySymbol = Symbol();
+
+// let person = {
+//     name: 'John',
+//     age: 32,
+//     partTime: false,
+//     [mySymbol]: 'secret',
+//     myMethod: function() {
+//         showMessage('Hello, ' + this.name);
+//     }
+// };
+
+// person.myMethod();
+// showMessage(person.name)
+
+// let button = document.getElementById("see-review");
+// const button_text = button.textContent;
+// button.addEventListener("click", function() {
+//     const review = document.getElementById("review");
+//     if (review.classList.contains("d-none")) {
+//         review.classList.remove("d-none");
+//         button.textContent = "Close review";
+//     } else {
+//         review.classList.add("d-none");
+//         button.textContent = button_text;
+//     }
+// });
+
+const values = [1,2,"3"];
---
+++ Wrappers/javascript/JS-course/Getting Started/05/demos/before/js-demo/utils.js
@@ -0,0 +1,7 @@
+function showMessage(message) {
+    document.getElementById('message').textContent = message;
+}
+
+function changePercentOff(percentage) {
+    document.getElementById('percentage').textContent = percentage + "% OFF";
+}
